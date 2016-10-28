ce5fce244932d486533bca0c2f9e6515c3a4cee9
Shweta <shwayduh@gmail.com>
2016-10-28 17:33:00 -0400

    90% done

--- APP/js/scripts.js
+++ APP/js/scripts.js
@@ -6,6 +6,37 @@ $( document ).ready(function() {
 	    }, function(){
 	    $(this).removeClass("animated infinite headShake");
 	})
+	//image fade out scroll
+	$(window).on("load",function() {
+  		$(window).scroll(function() {
+    		$(".hidden").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+      
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(300,1);}
+      } else { //object goes out of view (scrolling up)
+        if ($(this).css("opacity")==1) {$(this).fadeTo(300,0);}
+      }
+	    });
+	  }); $(window).scroll(); 
+	  //invoke scroll-handler on page-load
+	});
+
+	//project title fade scroll
+	$(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+            $('.project-title, .slogan').css({'opacity':(( 180-scroll )/100)+0.1});
+    });
+
+	// $(".skillname").hover(function(){
+	//     $(".skillsets").show("slow");
+	//     }, function(){
+	//     $(".skillsets").hide("slow");
+	// })
+
 
 	// thumbnail hover
 	// $('.thumb').on({
@@ -40,30 +71,6 @@ $( document ).ready(function() {
 	// 	})
 	// });
 
-	//image fade out scroll
-	$(window).on("load",function() {
-  		$(window).scroll(function() {
-    		$(".hidden").each(function() {
-      /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
-      
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(300,1);}
-      } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(300,0);}
-      }
-	    });
-	  }); $(window).scroll(); 
-	  //invoke scroll-handler on page-load
-	});
-
-	//project title fade scroll
-	$(window).scroll(function() {    
-        var scroll = $(window).scrollTop();
-            $('.project-title, .slogan').css({'opacity':(( 180-scroll )/100)+0.1});
-        });
 
 
 
